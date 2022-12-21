2df7503498982d9cc0fbcb518ac0606b40653d2e
정원철 <gksrud316@naver.com>
2022-12-21 16:53:16 +0900

    [22.12.21] 53: page routing test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import New from './pages/New';
+import Diary from './pages/Diary';
 import './App.css';
+import RouterTest from './components/RouterTest';
+
 
 function App() {
   return (
+<BrowserRouter>
     <div className="App">
-
+      <h1>app test</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/new" element={<New/>}/>
+        <Route path="/diary" element={<Diary/>}/>
+        <Route path="/edit" element={<Edit/>}/>
+      </Routes>
+      <RouterTest></RouterTest>
     </div>
+</BrowserRouter>
   );
 }
 
