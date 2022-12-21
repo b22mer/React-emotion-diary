9d6a6a5d49ec1b3ee0fe3ec0e481cc8d7eb6967d
정원철 <gksrud316@naver.com>
2022-12-21 17:40:34 +0900

    [22.12.21] 54: path variable, QS, page moving test

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,8 @@
-const Diary =()=>{
+import { useParams } from "react-router-dom";
 
+const Diary =()=>{
+    const {id}=useParams(); 
+    console.log(id);
     return(
         <div>
             <h1>Diary</h1>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit =()=>{
+
+    const [searchParams, setSearchParams]=useSearchParams();
+    const id=searchParams.get("id");
+    console.log(id);
+
+    const mode=searchParams.get("mode");
+    console.log(mode);
+    
+    const navigate=useNavigate();
 
     return(
         <div>
             <h1>Edit</h1>
             <p>이곳은 Edit 입니다.</p>
+            <button onClick={()=>setSearchParams({who:"woncheol"})}>QS Change</button>
+            <button onClick={()=>{navigate("/home")}}>Home으로가기</button>
+            <button onClick={()=>{navigate(-1)}}>뒤로가기</button>
+
+
         </div>
     )
 }
